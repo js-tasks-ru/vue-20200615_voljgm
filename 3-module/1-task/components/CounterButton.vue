8f8eb208d0803759511ff042182d2a7a37db7ff3
Vladimir Lyutetskiy <vl@webinnovations.ru>
2020-06-28 17:02:24 +0300

    task 3-1

---
+++ 3-module/1-task/components/CounterButton.vue
@@ -0,0 +1,23 @@
+<template>
+    <button type="button" @click="increment">{{ count }}</button>
+</template>
+
+<script>
+export default {
+    name: "CounterButton",
+    
+    props: {
+        count: {
+            type: Number,
+            default: 0
+        },
+    },
+
+  methods: {
+    increment() {
+      this.count += 1;
+      this.$emit('increment', this.count);
+    },
+  },
+}   
+</script>
